4ece1197d22f993a2428e15d4a2b031b330bddd4
rishabh <rbrishabh76@gmail.com>
2021-11-01 14:28:30 +0530

    and file completed

--- and.py
+++ and.py
@@ -1,5 +1,6 @@
+from os import PathLike
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_model,save_plot
 import pandas as pd 
 import numpy as np
 AND = {
@@ -9,6 +10,7 @@ AND = {
 }
 
 df = pd.DataFrame(AND)
+print(df)
 
 X,y = prepare_data(df)
 
@@ -19,3 +21,8 @@ model = Perceptron(eta=ETA, epochs=EPOCHS)
 model.fit(X, y)
 
 _ = model.total_loss()
+
+save_model(model,filename="and.model")
+save_plot(df,"and.png",model)
+
+
